721031b7bc7529b8ddf8c74e1764095ab9e57637
danteF <dante.kiki569@gmail.com>
2023-04-02 16:29:07 -0400

    configurando servidor y variables de entorno

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    
+    PORT: process.env.PORT,
+    SECRET_SESSION: process.env.SECRET_SESSION
+
+}
---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const expressSession = require('express-session');
+const expressHandlebars = require('express-handlebars');
+const cors = require('cors');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const path = require('path');
+require('dotenv').config();
+
+//init 
+const app = express();
+
+//settings
+app.set('port', require('./config/config').PORT || 4000);
+app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/public'));
+app.engine('.hbs', expressHandlebars.engine({
+
+    defaultLayout: 'main.hbs', 
+    layoutsDir: path.join(app.get('views'), '/layouts'),
+    partialsDir: path.join(app.get('views'), '/partials'),
+    extname: '.hbs'
+
+}));
+app.set('view engine'), 'hbs';
+
+//middlewares
+app.use(cors());
+app.use(morgan('dev'));
+app.use(expressSession({
+
+    secret: require('./config/config').SECRET_SESSION, 
+    resave: true, 
+    saveUninitialized: true
+
+}));
+app.use(methodOverride('_method'));
+
+//routes
+
+//public
+
+//server
+
+app.listen(app.get('port'), ()=>{
+
+    console.log("SERVER UP "+app.get('port'));
+
+});
